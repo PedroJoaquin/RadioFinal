5498dc753d703aef035622a42521166361ce9c64
PedroJoaquin <pedro_25@live.com>
2015-07-16 21:49:12 -0600

    Update Radio.java
    Se agregaron comentarios Java.doc

--- src/Radio.java
+++ src/Radio.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 //Pedro Joaquin Castillo Coronado 14224
-//Andre Josue Rodas Hern·ndez 14395
+//Andre Josue Rodas Hern√°ndez 14395
 //Freddy Josue Ruiz Gatica 14592
 //Algoritmos y Estructura de Datos
 //Seccion 10 
@@ -46,24 +46,43 @@ public class Radio implements InterfazRadio {
 	}
 	
 	/*Metodos get de los atributos*/
+	/**
+	 * @return the volumen
+	 */
 	public int getVolumen(){
 		return volumen;
 	}
+	/**
+	 * @return the emisora
+	 */
 	public double getEmisora(){
 		return emisora;
 	}
+	/**
+	 * @return the frecuencia
+	 */
 	public boolean getFrecuencia(){
 		return frecuencia;
 	}
+	/**
+	 * @return the encendido
+	 * 
+	 */
 	public boolean getEncendido(){
 		return encendido; 
 	}
+	/**
+	 * @return the memoria
+	 */
 	public double getMemoria(int fila, int columna){
 		return memoria[fila][columna];
 	}
 	
 	/*Metodos que son implementados de la InterfazRadio*/
 	/*Metodo que permite encender o apagar el radio*/
+	/* (non-Javadoc)
+	 * @see InterfazRadio#encenderApagar()
+	 */
 	public void encenderApagar(){
 		if(encendido==true){				//Si la radio esta encendida se apaga
 			encendido=false;
@@ -74,6 +93,10 @@ public class Radio implements InterfazRadio {
 	}
 	
 	/*Metodo para alternar la frecuencia entre AM y FM*/
+	
+	/* (non-Javadoc)
+	 * @see InterfazRadio#cambiarFrec()
+	 */
 	public void cambiarFrec(){
 		if(frecuencia==true){				//Si la radio esta en FM cambia a AM
 			frecuencia=false;	
@@ -86,6 +109,11 @@ public class Radio implements InterfazRadio {
 	}
 	
 	/*Metodo para cargar una emisora previamente guardada*/
+	/*Metodo para cargar una emisora previamente guardada*/
+	
+	/**
+	 * @param emisora
+	 */
 	public void cargarEmisora(double emisora){
 		if(frecuencia==true){								//Si la frecuencia esta en FM 									//se obtienen los datos de la primera fila del arreglo
 			this.emisora=memoria[0][(int) emisora-1];		//Se resta 1 a la emisora para encontrar la columna
@@ -96,6 +124,9 @@ public class Radio implements InterfazRadio {
 	}
 	
 	/*Metodo para adelantar la emisora*/
+	/* (non-Javadoc)
+	 * @see InterfazRadio#adelantarEmisora()
+	 */
 	public void adelantarEmisora(){
 		if(frecuencia==true){								//Si la frecuencia esta en FM: 							
 			if(emisora==MAX_FM){							//Si ya esta en la maxima emisora
@@ -116,6 +147,9 @@ public class Radio implements InterfazRadio {
 	}
 	
 	/*Metodo para atrasar la emisora*/
+	/* (non-Javadoc)
+	 * @see InterfazRadio#atrasarEmisora()
+	 */
 	public void atrasarEmisora(){
 		if(frecuencia==true){								//Si la frecuencia esta en FM: 							
 			if(emisora==MIN_FM){							//Si ya esta en la minima emisora
@@ -136,6 +170,9 @@ public class Radio implements InterfazRadio {
 	}
 	
 	/*Metodo para guardar emisora*/
+	/* (non-Javadoc)
+	 * @see InterfazRadio#guardarEmisora(double, int)
+	 */
 	public void guardarEmisora(double emisora, int posicion){
 		if(frecuencia==true){								//Si esta en frecuencia FM:
 			memoria[0][posicion-1]=this.emisora;			//Almacena la emisora actual en la primera fila 
@@ -146,6 +183,9 @@ public class Radio implements InterfazRadio {
 	}
 	
 	/*Metodo para subirVolumen*/
+	/* (non-Javadoc)
+	 * @see InterfazRadio#subirVolumen()
+	 */
 	public void subirVolumen(){
 		if(volumen!=10){
 			volumen++;
@@ -153,6 +193,10 @@ public class Radio implements InterfazRadio {
 	}
 	
 	/*Metodo para reducir el volumen*/
+	/*Metodo para reducir el volumen*/
+	/* (non-Javadoc)
+	 * @see InterfazRadio#bajarVolumen()
+	 */
 	public void bajarVolumen(){
 		if(volumen!=0){
 			volumen--;
